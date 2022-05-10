0e193975c690eba794412fc2a8ce67c056340e50
Nathan Provost <nprovost.pro@gmail.com>
2022-05-10 10:46:50 +0200

    1er style

--- client/components/Login.vue
+++ client/components/Login.vue
@@ -1,50 +1,61 @@
 <template>
-<form @submit="checkData">
-  <input type="text" name="email" v-model="login.email" placeholder="Entrez votre adresse-mail"/>
-  <input type="text" name="password" v-model="login.password" placeholder="Entrez votre mot de passe"/>
-  <button type="submit"> Connexion </button>
-</form>
-
+  <form @submit="checkData">
+    <input
+      type="text"
+      name="email"
+      v-model="login.email"
+      placeholder="Entrez votre adresse-mail"
+    />
+    <input
+      type="text"
+      name="password"
+      v-model="login.password"
+      placeholder="Entrez votre mot de passe"
+    />
+    <button type="submit">Connexion</button>
+  </form>
 </template>
 
 <script>
-
 module.exports = {
   name: "Login",
 
-  data () {
+  data() {
     return {
-      login : {
+      login: {
         email: "",
         password: "",
       },
-    }
+    };
   },
 
-  methods : {
-     async checkData() {
-       let user = {email : this.login.email, password : this.login.password}
+  methods: {
+    async checkData() {
+      let user = { email: this.login.email, password: this.login.password };
 
       try {
-        const response = await axios.post('/api/login', user)
-        console.log(response.data.success)
-        if(response.data.success === 1){
-          await this.$router.push({name: 'home'})
+        const response = await axios.post("/api/login", user);
+        console.log(response.data.success);
+        if (response.data.success === 1) {
+          await this.$router.push({ name: "home" });
         }
-        if(response.data.success === 0){
-          this.error = "Connexion Échouée"
+        if (response.data.success === 0) {
+          this.error = "Connexion Échouée";
         }
-
+      } catch (err) {
+        console.error("network error", err);
       }
-      catch(err) {console.error("network error",err)}
-    }
-  }
-}
-
-
+    },
+  },
+};
 </script>
 
 <style scoped>
+form {
+  margin: 1em;
+  display: column;
+}
+
 button {
   padding: 10px;
 }
